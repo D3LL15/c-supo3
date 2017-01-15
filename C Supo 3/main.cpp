cc56ab885ce65ecdffd36f1639f9030b275a6fa2
Daniel Ellis <de300@cam.ac.uk>
2017-01-15 16:11:24 +0000

    Initial Commit

---
+++ C Supo 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C Supo 3
+//
+//  Created by Daniel Ellis on 15/01/2017.
+//  Copyright © 2017 Daniel Ellis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
